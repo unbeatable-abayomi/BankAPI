a35b28f056717ac1b0bfd7e229f55f836c41cb5e
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-03-08 18:48:13 +0100

    Add project files.

---
+++ BankAPI/Controllers/BanksController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BankAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+{
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm"
+        };
+
+        private readonly ILogger<BanksController> _logger;
+
+        public BanksController(ILogger<BanksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
