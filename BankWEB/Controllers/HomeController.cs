9f6084ddc33e2be8d9301620c8e475233ef6ec76
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-03-09 01:34:11 +0100

    Set Up Start Up Project

--- BankWEB/Controllers/HomeController.cs
+++ BankWEB/Controllers/HomeController.cs
@@ -6,20 +6,71 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BankWEB.Models;
+using BankWEB.HelperClassData;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace BankWEB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        BankAPIHelper bankAPIHelper = new BankAPIHelper();
+        List<CorrectBankDetails> correctBankDetails = new List<CorrectBankDetails>();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            List<EmployeeBankDetails> bankDetails = new List<EmployeeBankDetails>();
+            HttpClient client = bankAPIHelper.Initial();
+
+            var helperDataClass = new BankAPIHelper();
+
+            var details = helperDataClass.GetAllEmployeeBankDetails();
+
+            foreach (var d in details.employee)
+            {
+                //string bankName = d.bankName;
+                HttpResponseMessage res = await client.GetAsync($"api/banks/{d.bankName}");
+                if (res.IsSuccessStatusCode)
+                {
+                    var results = res.Content.ReadAsStringAsync().Result;
+                    bankDetails = JsonConvert.DeserializeObject<List<EmployeeBankDetails>>(results);
+                    await Task.Run(() => {
+                        foreach (var x in bankDetails)
+                        {
+                            if (d.accountNumber == x.AccountNumber && d.bankName == x.BankName)
+                            {
+                                CorrectBankDetails correctBankDetails1 = new CorrectBankDetails()
+                                {
+                                    AccountNumber = x.AccountNumber,
+                                    BankName = x.BankName,
+                                    AccountName = x.AccountName
+                                };
+
+
+
+                                correctBankDetails.Add(correctBankDetails1);
+
+                            }
+                        }
+                    });
+
+                }
+
+
+            }
+            var allValidZenithBank = correctBankDetails.ToList();
+            // HttpResponseMessage res = await client.GetAsync("api/employee/{}");
+            //if (res.IsSuccessStatusCode)
+            //{
+            //    var results = res.Content.ReadAsStringAsync().Result;
+            //    bankDetails = JsonConvert.DeserializeObject<List<EmployeeBankDetails>>(results);
+            //}
             return View();
         }
 
--- BankWEB/HelperClassData/BankAPIHelper.cs
+++ BankWEB/HelperClassData/BankAPIHelper.cs
@@ -1,4 +1,5 @@
 ﻿using BankWEB.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
